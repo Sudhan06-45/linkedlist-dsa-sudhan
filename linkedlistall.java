00b5f471f3187d784d693693df48c38e0845def5
Sudhan06-45 <Sudhan.Suresh@WinWire.com>
2025-09-16 12:43:39 +0530

    Initial commit: linked list project skeleton

---
+++ linkedlistall.java
@@ -0,0 +1,140 @@
+package SLL;
+
+public class linkedlistall {
+    
+    public Node head;
+
+    // Traverse the linked list
+    public void traverse(){
+        Node curr = head;
+        while(curr!= null){
+            System.out.print(curr.data + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+
+    //Insert at the beginning
+    public void ins_beg(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head =newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    //Insert at the end
+    public void ins_end(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        Node curr = head;
+        while(curr.next!= null){
+            curr = curr.next;
+        }
+
+        curr.next = newNode;
+    }
+
+    //Insert at a given position
+    public void ins_specific(int data, int pos){
+        Node newNode = new Node(data);
+
+        if (head == null || pos == 0){
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+         
+        Node curr = head;
+        int count = 0;
+        while(curr!=null && count<pos -1 ){
+            curr = curr.next;
+            count++;
+        }
+        if(curr == null){
+            System.out.println("Position out of bounds");
+            return;
+        }
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+    }
+
+
+// Deletions
+    // Delete from the beginning
+
+    public void del_beg(){
+        if(head == null){
+            System.out.println("List is empty");
+            return; 
+        }
+        head = head.next;    
+    }
+
+    // Delete from the end
+    public void del_end(){
+        if(head == null){
+            System.out.println("List is empty");
+            return; 
+        }
+
+        if(head.next == null){
+            head = null;
+            return;
+        }
+
+        Node curr = head;
+        Node prev = null;
+
+        while(curr.next != null){
+            prev = curr;
+            curr = curr.next;
+        }
+
+        if (prev != null) {
+            prev.next = null;
+        }
+}
+
+    // Delete from a specific position
+    public void del_specific(int pos){
+        if(head == null){
+            System.out.println("List is empty");
+            return; 
+        }
+
+        if(pos == 0){
+            head = head.next;
+            return;
+        }
+
+        Node curr = head;
+        Node prev = null;
+        int count = 0;
+
+        while(curr != null && count < pos){
+            prev = curr;
+            curr = curr.next;
+            count++;
+        }
+
+        if (prev == null || curr == null) {
+            System.out.println("Position out of bounds");
+            return;
+        }
+
+        prev.next = curr.next;
+
+    }
+
+    // Search for an element
+    
+}
---
+++ main.java
@@ -0,0 +1,40 @@
+package SLL;
+
+public class main {
+    
+    public static void main(String[] args) {
+        
+        linkedlistall list = new linkedlistall();
+        
+        // Insertions
+        System.out.println("Linked List after Insertions:");
+        System.out.println("Insertion at begin:");
+        list.ins_beg(1);
+        list.ins_beg(0);
+        list.traverse();
+        System.out.println("Insertion at end:");
+        list.ins_end(2);
+        list .ins_end(4);
+        list.traverse();
+        System.out.println("Insertion at specific position:");
+        list.ins_specific(3, 3);
+        list.traverse();
+        // System.out.println(list.head.data); // Output: 0
+
+        // Deletions
+        System.out.println("Linked List after Deletions:");
+        System.out.println("Deletion at begin:");
+        list.del_beg();
+        list.traverse();
+
+        System.out.println("Deletion at end:");
+        list.del_end();
+        list.traverse();
+
+        System.out.println("Deletion at specific position:");
+        list.del_specific(1);
+        list.traverse();
+
+
+    }
+}
